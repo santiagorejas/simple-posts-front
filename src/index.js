c85585b9174e224af838ddd5c96fd8cc350962f5
santiagorejas <santiago.orejasrg@gmail.com>
2022-04-19 19:25:10 -0300

    using root.render()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import ProfileContextProvider from "./context/ProfileContextProvider";
 import AuthContextProvider from "./context/AuthContextProvider";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <AuthContextProvider>
       <ProfileContextProvider>
@@ -16,8 +17,7 @@ ReactDOM.render(
         </React.StrictMode>
       </ProfileContextProvider>
     </AuthContextProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
