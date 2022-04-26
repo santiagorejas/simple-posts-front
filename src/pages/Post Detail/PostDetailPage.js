4ea3e4a152c9cd0547fbefbe20567a53d5fcca6b
santiagorejas <santiago.orejasrg@gmail.com>
2022-04-26 19:38:52 -0300

    loading spinner added to post details

--- src/pages/Post Detail/PostDetailPage.js
+++ src/pages/Post Detail/PostDetailPage.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useHttp } from "../../hooks/use-http";
 import PostDetail from "../../components/Post Detail/PostDetail";
+import Section from "../../components/UI/Section";
+import LoadingSpinner from "../../components/UI/LoadingSpinner";
 
 const PostDetailPage = (props) => {
   const postId = useParams().pid;
@@ -31,7 +33,11 @@ const PostDetailPage = (props) => {
 
   return (
     <>
-      {isLoading && <h1>Loading...</h1>}
+      {(postDetail === null || isLoading) && (
+        <Section className={props.className}>
+          <LoadingSpinner />
+        </Section>
+      )}
       {!isLoading && postDetail !== null && (
         <PostDetail
           postId={postId}
