e7e670bbb19858c11198dbdeaf618874428e4e2f
santiagorejas <santiago.orejasrg@gmail.com>
2022-06-23 15:23:34 -0300

    app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { createBrowserHistory } from "history";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// const renderMockApp = (history) => {
+//     return render(
+//         <BrowserRouter location={history.location}>
+//             <App />
+//         </BrowserRouter>
+//     );
+// };
+
+describe("App", () => {
+    it("should render home (/post) when clicking the logo.", async () => {
+        const history = createBrowserHistory();
+        history.push("/login");
+
+        render(
+            <BrowserRouter location={history.location} navigator={history}>
+                <App />
+            </BrowserRouter>
+        );
+
+        const logoElement = screen.getByTestId("logo");
+
+        const user = userEvent.setup();
+
+        await user.click(logoElement);
+
+        const postsListTitleElement = await screen.findByTestId(
+            "posts-list-title"
+        );
+
+        expect(postsListTitleElement).toBeInTheDocument();
+    });
 });
